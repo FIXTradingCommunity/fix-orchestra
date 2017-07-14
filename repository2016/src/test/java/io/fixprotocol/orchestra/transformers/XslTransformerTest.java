3daa470d86e48991244627b8f1287dad0dcb0645
Don Mendelson <donmendelson@gmail.com>
2017-07-14 17:18:17 -0500

    Correct URI format, make it work on Windows

--- repository2016/src/test/java/io/fixprotocol/orchestra/transformers/XslTransformerTest.java
+++ repository2016/src/test/java/io/fixprotocol/orchestra/transformers/XslTransformerTest.java
@@ -34,8 +34,9 @@ public class XslTransformerTest {
         arr[1] = Thread.currentThread().getContextClassLoader().getResource("FixRepository.xml")
                 .getFile();
         String sourceDir = new File(arr[1]).getParent();
-        arr[2] = String.format("%s%soutput.xml", System.getProperty("java.io.tmpdir"), System.getProperty("file.separator"));
-        arr[3] = String.format("phrases-file=%s%sFIX.5.0SP2_EP216_en_phrases.xml", sourceDir, System.getProperty("file.separator"));
+        arr[2] = String.format("%s/%soutput.xml", System.getProperty("java.io.tmpdir"));
+        // document function in XSLT expects a URI, not a file name (Saxon does not convert) 
+        arr[3] = String.format("phrases-file=file:///%s/FIX.5.0SP2_EP216_en_phrases.xml", sourceDir.replace('\\', '/'));
         RepositoryXslTransformer.main(arr);
         File outFile = new File(arr[2]);
         Assert.assertTrue(outFile.exists());
