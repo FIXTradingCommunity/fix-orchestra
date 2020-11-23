6f8af144d1480e6eb9134c61cd0742c623046c7b
Don Mendelson <donmendelson@gmail.com>
2020-11-23 15:12:17 -0600

    Correct Java 11 module-info.java

--- repository-util/src/main/java11/module-info.java
+++ repository-util/src/main/java11/module-info.java
@@ -5,6 +5,7 @@ module orchestra.repository.util {
   requires org.apache.logging.log4j;
   requires java.xml;
   requires orchestra.score;
+  requires Saxon.HE;
 
   exports io.fixprotocol.orchestra.repository;
   exports io.fixprotocol.orchestra.transformers;
